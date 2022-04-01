1bc6499e461f4973617f1d55d3546bf3ae397e08
SusanBird <susanbird391@gmail.com>
2022-04-01 14:04:43 -0700

    netlify

--- fetch-utils.js
+++ fetch-utils.js
@@ -19,7 +19,7 @@ export async function getFamilies() {
 }
 
 export async function deleteBunny(id) {
-    // delete a single bunny using the id argument
+    // delete a single bunny using the id  argument
     const response = await client
         .from('fuzzy_bunnies')
         .delete()
